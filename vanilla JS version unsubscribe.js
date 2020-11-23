0f140d12055b266d291c32365f97eaef201f9b8f
CHARAN <cc710788@gmail.com>
2020-11-23 19:34:12 +0530

    Update vanilla JS version unsubscribe.js

--- vanilla JS version unsubscribe.js
+++ vanilla JS version unsubscribe.js
@@ -1,3 +1,6 @@
+// script for unsubscribe multiple channels in Youtube
+
+// Get the subscribed button
 let subscribeBtn = document.getElementsByTagName('paper-button')
 
 // Get the confirmatin button (unsubscribe button)
@@ -6,10 +9,12 @@ let confirmBtn =  document.querySelectorAll('#confirm-button')
 // Get the numbers of channels based on avatars
 let avatars = document.querySelectorAll('#avatar').length
 
+let channels = document.querySelectorAll('.style-scope ytd-channel-renderer')
+
 
 // This is Latest JS version Async/Await code
 async function timer(i){
-    if(i < avatars){
+    if(i < channels.length){
         await subscribeBtn[i].click()
         await confirmBtn[0].click()
         i++
